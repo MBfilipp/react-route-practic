50ec9a1b163a9f8768a2c26e837bb1cd89ad3fe4
Filipp <filippstryukop@gmail.com>
2019-12-09 22:29:55 +0200

    Updates

---
+++ precache-manifest.edd28da4c464bf31fa6bfa718d6c0644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4339d928cd1fbd6225c58ec8c3e03a34",
+    "url": "/react-route-practic/index.html"
+  },
+  {
+    "revision": "2fc9f740981c6d96ca9d",
+    "url": "/react-route-practic/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4c616f7af61f80975238",
+    "url": "/react-route-practic/static/js/2.7884f76c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-route-practic/static/js/2.7884f76c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fc9f740981c6d96ca9d",
+    "url": "/react-route-practic/static/js/main.f70beea6.chunk.js"
+  },
+  {
+    "revision": "cf06ea86d8f2ae50a12b",
+    "url": "/react-route-practic/static/js/runtime-main.99d97184.js"
+  }
+]);
